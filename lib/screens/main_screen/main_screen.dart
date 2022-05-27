f6ae3ec71df31ed82479d326b1c8a0756345b5b9
gooodwin67 <sn67@inbox.ru>
2022-05-27 22:02:46 +0300

    slivers

--- lib/screens/main_screen/main_screen.dart
+++ lib/screens/main_screen/main_screen.dart
@@ -17,20 +17,51 @@ class ListBanners {
   ListBanners({required this.image});
 }
 
-class MainScreenWidget extends StatelessWidget {
+class MainScreenWidget extends StatefulWidget {
   const MainScreenWidget({Key? key}) : super(key: key);
+
+  @override
+  State<MainScreenWidget> createState() => _MainScreenWidgetState();
+}
+
+class _MainScreenWidgetState extends State<MainScreenWidget> {
+  var scrollController = ScrollController();
+  double newOpacity = 1;
+  void _onScrollEvent() {
+    final extentOpacity;
+    extentOpacity = scrollController.position.extentBefore;
+    if (extentOpacity < 150) {
+      newOpacity = (150.0 - extentOpacity) / 150;
+    } else {
+      newOpacity = 0;
+    }
+    setState(() {});
+
+    print("Extent after: $newOpacity");
+  }
+
+  @override
+  void initState() {
+    scrollController.addListener(_onScrollEvent);
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
     final Size size = MediaQuery.of(context).size;
+
     return Scaffold(
       body: SafeArea(
         child: CustomScrollView(
+          controller: scrollController,
           slivers: <Widget>[
             SliverAppBar(
               titleSpacing: 0,
               backgroundColor: Colors.white,
-              title: Banners_List_Wrap(size: size, listBanners: listBanners),
+              title: Opacity(
+                opacity: newOpacity,
+                child: Banners_List_Wrap(size: size, listBanners: listBanners),
+              ),
               //expandedHeight: 150,
               //collapsedHeight: 150,
               toolbarHeight: size.height * 0.25 + kDefaultPadding,
@@ -44,9 +75,14 @@ class MainScreenWidget extends StatelessWidget {
             SliverList(
               delegate: SliverChildBuilderDelegate(
                 (BuildContext context, int index) {
-                  return Container(
-                    height: 20,
-                    color: Colors.red,
+                  return Column(
+                    children: [
+                      Container(
+                        height: 20,
+                        color: Colors.red,
+                      ),
+                      SizedBox(height: 5),
+                    ],
                   );
                 },
                 childCount: 100, // 1000 list items
